ef81c711ad15d4396527b7c920f751fb85aa756f
rutbel.huapaya <rhuapayaa@pucp.edu.pe>
2024-05-26 09:37:18 -0500

    Add more constants

--- src/index.ts
+++ src/index.ts
@@ -57,6 +57,14 @@ const urlEconomia = {
 			url: "https://elcomercio.pe/arcio/rss/category/economia/lec/",
 			brand: "elcomercio",
 		},
+		{
+			url: "https://feeds.elpais.com/mrss-s/pages/ep/site/elpais.com/section/economia/portada",
+			brand: "elpais",
+		},
+		{
+			url: "https://www.abc.es/rss/2.0/economia/",
+			brand: "abc",
+		},
 	],
 };
 const urlPolitica = {
@@ -233,6 +241,38 @@ const urlMundo = {
 			url: "https://elcomercio.pe/arcio/rss/category/mundo/centroamerica/",
 			brand: "elcomercio",
 		},
+		{
+			url: "https://feeds.elpais.com/mrss-s/pages/ep/site/elpais.com/section/internacional/portada",
+			brand: "elpais",
+		},
+		{
+			url: "https://feeds.elpais.com/mrss-s/pages/ep/site/elpais.com/section/argentina/portada",
+			brand: "elpais",
+		},
+		{
+			url: "https://feeds.elpais.com/mrss-s/pages/ep/site/elpais.com/section/mexico/portada/",
+			brand: "elpais",
+		},
+		{
+			url: "https://feeds.elpais.com/mrss-s/pages/ep/site/elpais.com/section/america-colombia/portada",
+			brand: "elpais",
+		},
+		{
+			url: "https://feeds.elpais.com/mrss-s/pages/ep/site/elpais.com/section/chile/portada",
+			brand: "elpais",
+		},
+		{
+			url: "https://www.abc.es/rss/2.0/espana/madrid/",
+			brand: "abc",
+		},
+		{
+			url: "https://www.abc.es/rss/2.0/espana/",
+			brand: "abc",
+		},
+		{
+			url: "https://www.abc.es/rss/2.0/internacional/",
+			brand: "abc",
+		},
 	],
 };
 const urlBienestar = {
@@ -256,6 +296,22 @@ const urlBienestar = {
 		{
 			url: "https://elcomercio.pe/arcio/rss/category/artlima/mente-sana/",
 			brand: "elcomercio",
+		},
+		{
+			url: "https://feeds.elpais.com/mrss-s/list/ep/site/elpais.com/section/mamas-papas",
+			brand: "elpais",
+		},
+		{
+			url: "https://www.abc.es/rss/2.0/bienestar/alimentacion/",
+			brand: "abc",
+		},
+		{
+			url: "https://www.abc.es/rss/2.0/bienestar/fitness/",
+			brand: "abc",
+		},
+		{
+			url: "https://www.abc.es/rss/2.0/bienestar/psicologia-sexo/",
+			brand: "abc",
 		},
 	],
 };
@@ -309,6 +365,14 @@ const urlTecnologia = {
 			url: "https://elcomercio.pe/arcio/rss/category/tecnologia/streamers/",
 			brand: "elcomercio",
 		},
+		{
+			url: "https://feeds.elpais.com/mrss-s/pages/ep/site/elpais.com/section/tecnologia/portada",
+			brand: "elpais",
+		},
+		{
+			url: "https://www.abc.es/rss/2.0/tecnologia/",
+			brand: "abc",
+		},
 	],
 };
 const urlDeporte = {
@@ -345,6 +409,34 @@ const urlDeporte = {
 			url: "https://elcomercio.pe/arcio/rss/category/deporte-total/voley/",
 			brand: "elcomercio",
 		},
+		{
+			url: "https://feeds.elpais.com/mrss-s/pages/ep/site/elpais.com/section/deportes/portada",
+			brand: "elpais",
+		},
+		{
+			url: "https://www.abc.es/rss/2.0/deportes/real-madrid/",
+			brand: "abc",
+		},
+		{
+			url: "https://www.abc.es/rss/2.0/deportes/tenis/",
+			brand: "abc",
+		},
+		{
+			url: "https://www.abc.es/rss/2.0/deportes/atletico-madrid/",
+			brand: "abc",
+		},
+		{
+			url: "https://www.abc.es/rss/2.0/deportes/futbol/",
+			brand: "abc",
+		},
+		{
+			url: "https://www.abc.es/rss/2.0/deportes/ciclismo/",
+			brand: "abc",
+		},
+		{
+			url: "https://www.abc.es/rss/2.0/deportes/vela/",
+			brand: "abc",
+		},
 	],
 };
 const urlGastronomia = {
@@ -368,6 +460,27 @@ const urlGastronomia = {
 		{
 			url: "https://elcomercio.pe/arcio/rss/category/gastronomia/internacional/",
 			brand: "elcomercio",
+		},
+		{
+			url: "https://feeds.elpais.com/mrss-s/list/ep/site/elpais.com/section/gastronomia",
+			brand: "elpais",
+		},
+	],
+};
+
+const urlCiencia = {
+	ciencia: [
+		{
+			url: "https://www.bbc.com/mundo/temas/ciencia/index.xml",
+			brand: "bbc",
+		},
+		{
+			url: "https://feeds.elpais.com/mrss-s/pages/ep/site/elpais.com/section/ciencia/portada",
+			brand: "elpais",
+		},
+		{
+			url: "https://www.abc.es/rss/2.0/ciencia/",
+			brand: "abc",
 		},
 	],
 };
@@ -406,6 +519,7 @@ const app = new Elysia()
 	.use(cronMapper("tecnologia", urlTecnologia))
 	.use(cronMapper("deporte", urlDeporte))
 	.use(cronMapper("gastronomia", urlGastronomia))
+	.use(cronMapper("ciencia", urlCiencia))
 	.get("/getEconomia", async () => {
 		await crons(urlEconomia);
 	})
@@ -429,6 +543,9 @@ const app = new Elysia()
 	})
 	.get("/getGastronomia", async () => {
 		await crons(urlGastronomia);
+	})
+	.get("/getCiencia", async () => {
+		await crons(urlCiencia);
 	})
 	.listen(3000);
 
